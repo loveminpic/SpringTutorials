63fcf7fc79224ab6bde3da57cbb6db8b8318adc2
Minji Lee <43211858+loveminpic@users.noreply.github.com>
2023-02-05 12:39:41 +0900

    connected DB with jdbctemplate

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -2,9 +2,6 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,13 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
-   @Autowired MemberService memberService;
-  @Autowired MemberRepository memberRepository;
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
     @Test
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("hello");
         //when
         Long savedId = memberService.join(member);
 
@@ -32,7 +30,7 @@ class MemberServiceIntegrationTest {
     }
 
     @Test
-    public void 중복회원예외(){
+    public void 중복_회원_예외(){
         //given
         Member member1 = new Member();
         member1.setName("spring");
@@ -41,14 +39,9 @@ class MemberServiceIntegrationTest {
         member2.setName("spring");
 
         memberService.join(member1);
-        assertThrows(IllegalStateException.class, ()-> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2)); //예외가 발생해야 한다.
 
-//        try{
-//            memberService.join(member2);
-//            fail();
-//        }catch (IllegalStateException e){
-//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//        }
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
     @Test
